c13ce94525a7c60650459a510815034df22b06e4
x14552067 <paulreid96@gmail.com>
2018-05-02 14:53:54 +0100

    Backup before trying to override registrations to create a student in the same form

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,20 @@ class RegistrationsController < Devise::RegistrationsController
   def after_sign_up_path_for(resource)
     '/students/new/' # Or :prefix_to_your_route
   end
+
+  def new
+    @signup = Signup.new
+  end
+
+  def create
+    @signup = Signup.new(params[:signup])
+
+    if @signup.save
+      sign_in @signup.user
+      redirect_to projects_path, notice: 'You signed up successfully.'
+    else
+      render action: :new
+    end
+  end
+
 end
