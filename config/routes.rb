5232b987b6ba6b9d3791d9f9f70673377f4b6935
x14552067 <paulreid96@gmail.com>
2018-05-02 12:01:41 +0100

    Added authentication to the applicaiton.
    Made it so the static pages controller doesnt authenticate the user.
    This will allow users to view landing page etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { registrations: "registrations", sign_in: "login" }
 
   authenticate :user do
-    get 'static_pages/home'
   end
-    root 'static_pages#home'
+
+  get 'static_pages/index'
+  devise_for :users, controllers: { registrations: "registrations", sign_in: "login" }
+  root 'static_pages#index'
+
 end
