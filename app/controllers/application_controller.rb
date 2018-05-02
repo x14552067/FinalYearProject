0c6e1bda94dd786f0185f48d5e67b0b1d99604a7
x14552067 <paulreid96@gmail.com>
2018-05-02 13:57:02 +0100

    Fixed Devise Forms
    Added Custom Styling for Login and Registration
    
    About to Add Registration Step 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-
-
-
+  def after_sign_in_path_for(resource_or_scope)
+    '/static_pages/index' # Or :prefix_to_your_route
+  end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,4 +1,6 @@
-class RegistrationsController < ApplicationController
+class RegistrationsController < Devise::RegistrationsController
+  protected
+
   def after_sign_up_path_for(resource)
     '/students/new/' # Or :prefix_to_your_route
   end
