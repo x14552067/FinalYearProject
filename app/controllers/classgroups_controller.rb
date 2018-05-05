3da70da10618a7db25413de189979c55322dfef9
x14552067 <paulreid96@gmail.com>
2018-05-05 11:52:59 +0100

    Added a List of Classes for a Student
    This can be accessed through the Dashboard

--- app/controllers/classgroups_controller.rb
+++ app/controllers/classgroups_controller.rb
@@ -1,5 +1,17 @@
 class ClassgroupsController < ApplicationController
 
+  def index
+
+    if current_user.lecturer.nil?
+      @classes = current_user.student.classgroups
+      render 'student_view'
+    else
+      @classes = current_user.lecturer.classgroups
+      render 'lecturer_view'
+    end
+
+  end
+
   def show
     @classgroup = Classgroup.find(params[:id])
     p @classgroup
