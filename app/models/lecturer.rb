e4b9c2b5cb40dd6b3d5580bea355ea3804770dc9
x14552067 <paulreid96@gmail.com>
2018-05-13 22:31:56 +0100

    GOING FOR YOLO BOYS

--- app/models/lecturer.rb
+++ app/models/lecturer.rb
@@ -2,6 +2,7 @@ class Lecturer < ApplicationRecord
   belongs_to :user
   has_many :classgroups
   has_many :chatmessages
+  has_many :answermessages
 
   validates :first_name, presence: true, length: {minimum: 2}
   validates :last_name, presence: true, length: {minimum: 2}
